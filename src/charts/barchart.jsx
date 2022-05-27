36e871867c37c56ed4234b4d5ab180d2710303e2
nikeshnaik <nikeshnaik.dev@gmail.com>
2022-05-27 19:22:23 +0530

    feat: Use Konva over Canvas to build barchart

--- src/charts/barchart.jsx
+++ src/charts/barchart.jsx
@@ -1,56 +1,78 @@
-import { useEffect, useRef } from 'react'
+import React, { useRef, useState } from 'react';
+import { Stage, Layer, Rect, Text, Label, Tag } from 'react-konva';
 
 function BarChart(props) {
 
-    const ref = useRef()
 
-    const barColor = "#EC755D"
+    const [showToolTip, setShowToolTip] = useState(new Array(6).fill(false))
+
+    const [dimension, setDimension] = useState({ width: 460, height: 220 })
+
+    const barNormalColor = "#EC755D"
     const maxValueBarColor = "#76B5BC"
-    let dpr = window.devicePixelRatio * 1.5
-
-    useEffect(() => {
-
-        var data = [...props.data]
-        var canvas = ref.current.getContext('2d')
-        canvas.scale(dpr, dpr);
-        var canvas_height = 178 * dpr
-        var offset = 3
-        var width = 30.92
-        var maxValue = 0
-        maxValue = Math.max(...data.map(item => item.amount))
+    var barColor = barNormalColor
+    const maxValue = Math.max(...props.data.map(item => item.amount))
+    var offset = 3
+    var width = 50.6
+    var height = dimension.height
+    var dataScale = 2.5
 
 
-        data.forEach(element => {
-            drawBar(canvas, offset, canvas_height - (element.amount * 1.5) - 170, width, (element.amount * 1.5), maxValue === element.amount)
-            fillTextBelowBar(canvas, offset + 7, canvas_height - 150, element.day)
-            offset = offset + width + 13
-        });
-    }, [dpr, props.data])
-
-
-    function drawBar(canvas, posX, posY, width, height, isItMaxValue) {
-        canvas.beginPath();
-        canvas.lineWidth = 0.0001
-        canvas.fillStyle = isItMaxValue ? maxValueBarColor : barColor
-        canvas.fillRect(posX, posY, width, height)
+    const handleMouserOver = (id, event) => {
+        let newState = new Array(6).fill(false)
+        newState[id] = true
+        setShowToolTip(newState)
     }
 
-    function fillTextBelowBar(canvas, posX, posY, textValue) {
-        canvas.font = "10px DM Sans";
-        canvas.fillStyle = "#92857A";
-        canvas.fillText(textValue, posX, posY);
-    }
+    const scale = Math.min(
+        window.innerWidth / dimension.width,
+        window.innerHeight / dimension.height
+    );
 
 
-    const handleHover = (event) => {
+    return (
+        <Stage width={dimension.width} height={height} >
+            <Layer>
+                {
+                    props.data.map((item) => {
+                        if (item.amount === maxValue) {
+                            barColor = maxValueBarColor
+                        }
+                        else {
+                            barColor = barNormalColor
+                        }
+                        var elem = <React.Fragment key={item.id}>
+                            <Rect
+                                x={offset}
+                                y={height - 25 - (item.amount * dataScale)}
+                                width={width}
+                                height={item.amount * dataScale}
+                                fill={barColor}
+                                cornerRadius={5} onMouseOver={(event) => handleMouserOver(item.id, event)}
+                            />
+                            <Text
+                                x={offset + 12}
+                                y={height - 155}
+                                fill={"#92857A"}
+                                text={item.day}
+                                fontSize={15}
+                                fontFamily={"DM Sans"}
+                                lineHeight={19.53}
+                            />
+                            {showToolTip[item.id] ?
+                                <Label x={offset + 2} y={height - 55 - item.amount * dataScale}>
+                                    <Tag cornerRadius={5} fill="black" />
+                                    <Text x={offset + 2} y={height - 55 - item.amount * dataScale} padding={6} text={'$' + item.amount} fill={'white'} fontSize={12} fontFamily={"DM Sans"} />
+                                </Label> : null}
+                        </React.Fragment>
+                        offset = offset + width + 16
+                        return elem
+                    })
+                }
+            </Layer>
+        </Stage >
+    )
 
-        // console.log(event.clientX, event.clientY, event.target)
-        event.stopPropagation()
-
-    }
-
-
-    return <canvas onMouseMove={handleHover} ref={ref} width={460 * dpr} height={178 * dpr} style={{ width: "46rem", height: "17.8rem" }} />
 }
 
 
